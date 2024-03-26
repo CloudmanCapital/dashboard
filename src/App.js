6d73502fc6543a744cc650a6fb9e8e8fde266f07
BANANAPEEL202 <bbob52645@gmail.com>
2024-03-26 11:36:24 -0400

    added header

--- src/App.js
+++ src/App.js
@@ -9,12 +9,21 @@ function App() {
         style={{ width: '12%', height: '40%', padding: '60px'}}/>
         <button class="sign-out-button">Sign Out</button>
       </header>
-      <div> // Main body
-        <div> // Left side (graph)
-          
+      <div> {/* Main body */}
+      <div class="horizontal-div">
+        <div class="left-align-container"> {/*Left side (graph)*/}
+            <div class="horizontal-div">
+              <div>
+                <h1 style={{ fontSize: "2rem" }}>Aditya Behera</h1>
+              </div>
+              <div class="rounded-rectangle">
+                +$3.08 (+48.2%) today
+              </div>
+            </div>
         </div>
-        </div>/ / Right side (account info)
+        </div> {/*Right side (account info) */}
         <div> 
+        </div>
       </div>
     </div>
   );
