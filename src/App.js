a505fad8f740de786f547e1b462939e819c5764c
BANANAPEEL202 <bbob52645@gmail.com>
2024-03-25 21:22:20 -0400

    did shit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import logo from './assets/Cloudman Capital Logo Transparent.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} alt="logo" 
+        style={{ width: '12%', height: '40%', padding: '60px'}}/>
+        <button class="sign-out-button">Sign Out</button>
       </header>
+      <div> // Main body
+        <div> // Left side (graph)
+          
+        </div>
+        </div>/ / Right side (account info)
+        <div> 
+      </div>
     </div>
   );
 }
